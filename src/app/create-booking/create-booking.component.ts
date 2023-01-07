f71df29460e84b84806c4497631c35af3a4f8f9b
AngelikaMann <99369663+AngelikaMann@users.noreply.github.com>
2023-01-07 04:34:29 +0100

    Daten mit FormGroup eingerichtet

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -3,6 +3,8 @@ import { Booking } from '../booking';
 // import { Bookings } from '../mock-bookings';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BookingService } from '../booking.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-booking',
@@ -13,8 +15,8 @@ export class CreateBookingComponent implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private bookingService: BookingService
-  ) {}
+    private bookingService: BookingService,
+    private formbuilder : FormBuilder) {}
 
   booking: Booking = {
     id: 10,
@@ -22,19 +24,49 @@ export class CreateBookingComponent implements OnInit {
     roomNumber: 105,
     startDate: new Date(),
     endDate: new Date(),
-  };
+  }
+  bookingForm :FormGroup= this.formbuilder.group({
+    id: ['', Validators.required],
+    roomNumber: ['', Validators.required],
+    name: ['', Validators.required],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required]
+
+  });
 
   ngOnInit(): void {
     if (this.router.url != '/create') {
       var id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
       this.bookingService.getBookingById(id).subscribe((result) => {
+
         this.booking = result;
+
+     this.bookingForm.setValue(
+        { id: this.booking.id,
+          name: this.booking.name,
+          roomNumber: this.booking.roomNumber,
+
+          startDate: this.booking.startDate,
+          endDate: this.booking.endDate
+        }
+   );
       });
     }
   }
+
+
+
   save(): void {
 
+     this.booking.id = this.bookingForm.get('id')?.value;
+     this.booking.roomNumber = this.bookingForm.get('roomNumber')?.value;
+     this.booking.name = this.bookingForm.get('name')?.value;
+     this.booking.startDate = this.bookingForm.get('startDate')?.value ;
+     this.booking.endDate = this.bookingForm.get('endDate')?.value ;
+
       this.bookingService.addBooking(this.booking).subscribe();
+
+
     this.router.navigate(['bookings']);
   }
 
